057dc035955c2fc275946124b7e402f51700e44d
thetygerpotatto <letras.cj@gmail.com>
2026-05-22 22:07:57 -0500

    feat: added jakarta percistency, and capacity for every type of ticket

--- src/main/java/com/vivaeventos/event_service/domain/model/Event.java
+++ src/main/java/com/vivaeventos/event_service/domain/model/Event.java
@@ -1,18 +1,31 @@
 package com.vivaeventos.event_service.domain.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 @Getter
+@Setter
 public class Event {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID eventId;
+
     private String name;
     private String description;
     private LocalDateTime date;
     private String Address;
-    private UUID eventId;
-
+    private Integer general_capacity;
+    private Integer vip_capacity;
+    private Integer student_capacity;
     
 }
