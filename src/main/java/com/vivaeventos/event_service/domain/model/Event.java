b4020edefaf0da2ca7da3c0c5726815a1a53be59
thetygerpotatto <letras.cj@gmail.com>
2026-05-23 16:28:39 -0500

    Feat: Added constraints for the fields

--- src/main/java/com/vivaeventos/event_service/domain/model/Event.java
+++ src/main/java/com/vivaeventos/event_service/domain/model/Event.java
@@ -6,10 +6,13 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -20,20 +23,42 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID eventId;
 
+    @NotEmpty(message ="No puede estar vacio")
+    @Size(min=2, max=20, message="El tamaño tiene que estar entre 2 y 20")
+    @Column(nullable=false)
     private String name;
+
+    @NotEmpty(message ="No puede estar vacio")
+    @Size(min=2, max=20, message="El tamaño tiene que estar entre 2 y 20")
+    @Column(nullable=false)
     private String description;
+
+    @Column(nullable=false)
     private LocalDateTime date;
+
+    @NotEmpty(message ="No puede estar vacio")
+    @Size(min=2, max=20, message="El tamaño tiene que estar entre 2 y 20")
+    @Column(nullable=false)
     private String address;
 
+    @Column(nullable=false)
     private Integer general_capacity;
+    @Column(nullable=false)
     private Integer vip_capacity;
+    @Column(nullable=false)
     private Integer student_capacity;
     
+    @Column(nullable=false)
     private Integer general_tickets_sold;
+    @Column(nullable=false)
     private Integer vip_tickets_sold;
+    @Column(nullable=false)
     private Integer student_tickets_sold;
 
+    @Column(nullable=false)
     private Double general_price;
+    @Column(nullable=false)
     private Double vip_price;
+    @Column(nullable=false)
     private Double student_price;
 }
