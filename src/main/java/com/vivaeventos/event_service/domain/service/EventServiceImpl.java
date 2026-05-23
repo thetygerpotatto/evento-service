3511b19d99cdcf07a88930f9379be7400478b7d8
thetygerpotatto <letras.cj@gmail.com>
2026-05-23 16:53:47 -0500

    Feat: EventServieImpl done for now

---
+++ src/main/java/com/vivaeventos/event_service/domain/service/EventServiceImpl.java
@@ -0,0 +1,45 @@
+package com.vivaeventos.event_service.domain.service;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+
+import com.vivaeventos.event_service.domain.model.Event;
+import com.vivaeventos.event_service.domain.repository.IEventRepository;
+/**
+ * EventServiceImpl
+ */
+public class EventServiceImpl implements IEventService {
+    
+    IEventRepository eventRepository;
+
+    public EventServiceImpl(IEventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Event save(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public void delete(Event event) {
+        eventRepository.delete(event);
+    }
+
+    @Override
+    public Optional<Event> findById(UUID id) {
+        return eventRepository.findById(id);
+    }
+
+    @Override
+    public Event update(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public List<Event> findAll() {
+        return eventRepository.findAll();
+    }
+    
+}
--- src/main/java/com/vivaeventos/event_service/domain/service/IEventService.java
+++ src/main/java/com/vivaeventos/event_service/domain/service/IEventService.java
@@ -3,11 +3,12 @@ package com.vivaeventos.event_service.domain.service;
 import com.vivaeventos.event_service.domain.model.Event;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface IEventService {
-    Event save(Event producto);
-    void delete(Event producto);
-    Optional<Event> findById(Long id);
+    Event save(Event event);
+    void delete(Event event);
+    Optional<Event> findById(UUID id);
     Event update(Event event);
     List<Event> findAll();
 }
