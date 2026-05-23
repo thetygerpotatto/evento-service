0f7d163464783f733fd340fd5631957cf8bf2c65
thetygerpotatto <letras.cj@gmail.com>
2026-05-22 22:22:09 -0500

    Feat: Create interface for the event repository

--- src/main/java/com/vivaeventos/event_service/domain/repository/IEventRepository.java
+++ src/main/java/com/vivaeventos/event_service/domain/repository/IEventRepository.java
@@ -1,2 +1,8 @@
+package com.vivaeventos.event_service.domain.repository;
 
+import java.util.UUID;
+import com.vivaeventos.event_service.domain.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEventRepository extends JpaRepository<Event, UUID> {}
 
