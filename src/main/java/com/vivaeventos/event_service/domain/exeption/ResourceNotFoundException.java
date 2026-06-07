5f03dd5a2aa08460441e118524d5bc3868782fad
thetygerpotatto <letras.cj@gmail.com>
2026-06-07 16:36:34 -0500

    Feat: new exeption

---
+++ src/main/java/com/vivaeventos/event_service/domain/exeption/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vivaeventos.event_service.domain.exeption;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
